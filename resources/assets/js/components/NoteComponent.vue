778cb3cc3b6d6762b468b9f3884ec7b1617dca62
Jyrone Parker <jyrone.parker@gmail.com>
2018-04-11 18:59:42 -0400

    refactor

--- resources/assets/js/components/NoteComponent.vue
+++ resources/assets/js/components/NoteComponent.vue
@@ -29,7 +29,6 @@
         data(){
           return{
             notes:[],
-            interimTranscript: '',
             transcipt: '',
             isRecording: false,
             speechRecog: {}
@@ -42,22 +41,18 @@
           });
           this.speechRecog = new webkitSpeechRecognition();
           this.speechRecog.continuous = true;
-          this.speechRecog.interimResults = true;
+          this.speechRecog.interimResults = false;
           this.speechRecog.onstart = function() {
             this.isRecording = true;
           }
 
           this.speechRecog.onresult = function(event) {
-            that.interimResults = '';
-            for (var i = event.resultIndex; i < event.results.length; ++i) {
-             if (event.results[i].isFinal) {
-               if(event.results[i][0].transcript != 'undefined')
+            var last = event.results.length - 1;
+             if (event.results[last].isFinal) {
+               if(event.results[last][0].transcript !== undefined)
                this.transcript += event.results[i][0].transcript;
-             } else {
-               if(event.results[i][0].transcript != 'undefined')
-               that.interimTranscript += event.results[i][0].transcript;
              }
-           }
+
 
           }
           this.speechRecog.onerror = function(event) {
@@ -70,7 +65,6 @@
             axios.post('/api/note',data).then(function(data){
               that.notes.push(data.data);
               that.transcipt = '';
-              that.interimResults = '';
             });
           }
         },
